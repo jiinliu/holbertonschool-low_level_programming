59a7c41de172998ce9d7ae62d385865d7ca2c430
Jin Liu <iamjincn@outlook.com>
2025-02-19 11:33:48 +0000

    task 3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: Prints the alphabet in lowercase, then in uppercase,
+ *              followed by a new line.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	putchar('a');
+	putchar('A');
+	putchar('\n');
+
+	return (0);
+}
+
