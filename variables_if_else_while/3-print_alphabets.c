7dd65649f248162576478edbc65ef103422553ec
Jin Liu <iamjincn@outlook.com>
2025-02-19 11:46:03 +0000

    task 3

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -8,8 +8,21 @@
  */
 int main(void)
 {
-	putchar('a');
-	putchar('A');
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+
+	letter = 'A';
+	while (letter <= 'Z')
+	{
+		putchar(letter);
+		letter++;
+	}
+
 	putchar('\n');
 
 	return (0);
