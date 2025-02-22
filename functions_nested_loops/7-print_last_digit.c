309076eae4e51d35dc690f010c3ea0250edc0334
Jin Liu <iamjincn@outlook.com>
2025-02-22 10:52:47 +0000

    task 7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Returns the value of the last digit
+ * @n: number
+ *
+ * Return: digit
+ */
+int print_last_digit(int n)
+{
+	int last_digit
+
+	last_digit = n % 10
+	if (n < 0)
+		last_digit = -last_digit;
+
+	return (last_digit);
+}
