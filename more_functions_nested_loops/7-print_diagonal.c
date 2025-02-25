ce6a5db8e56958ce9b6dc0621b2d7c5b4f0e019d
Jin Liu <iamjincn@outlook.com>
2025-02-25 11:16:35 +0000

    task 7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_diagonal  - function
+ * @n: The number of times the character \ should be printed
+ *
+ *
+ * return int
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
