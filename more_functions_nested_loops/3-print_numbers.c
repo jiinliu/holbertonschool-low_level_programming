870f837c203805897a498aa5be034a7a8f796dbe
Jin Liu <iamjincn@outlook.com>
2025-02-25 09:34:27 +0000

    task 3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - function
+ *
+ *
+ *
+ * Return: void
+ */i
+void print_numbers(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+		_putchar(num);
+
+	__putchar('\n');
+}
