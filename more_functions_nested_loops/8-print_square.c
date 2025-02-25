405f6b0659d36ae5058c8b0d9f6859bc6fcb2d50
Jin Liu <iamjincn@outlook.com>
2025-02-25 11:25:37 +0000

    task 8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_square  - function
+ * @size:the size of the square
+ *
+ *
+ * return void
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 0; size < ; i++)
+		{
+			for (j = 0; j < size; j++)
+				_putchar('#');
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
