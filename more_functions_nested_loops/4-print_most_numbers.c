902b0928320569a778c11bd875d90afb61b8eadb
Jin Liu <iamjincn@outlook.com>
2025-02-25 09:51:08 +0000

    task 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - function
+ *
+ *
+ *
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+		if (num != 4 && num != 2)
+			_putchar(num);
+
+	_putchar('\n');
+}
