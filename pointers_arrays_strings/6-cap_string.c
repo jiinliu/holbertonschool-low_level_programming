86c8d65aabe1a085eb1ba2341a8361fc00cc9cb3
Jin Liu <iamjincn@outlook.com>
2025-03-01 06:31:49 +0000

    task 6

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @str: the string to modify
+ *
+ * Return: the modified string
+ */
+char *cap_string(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' || 
+		    str[i - 1] == '\n' || str[i - 1] == ',' || str[i - 1] == ';' ||
+		    str[i - 1] == '.' || str[i - 1] == '!' || str[i - 1] == '?' ||
+		    str[i - 1] == '"' || str[i - 1] == '(' || str[i - 1] == ')' ||
+		    str[i - 1] == '{' || str[i - 1] == '}')
+		{
+			if (str[i] >= 'a' && str[i] <= 'z')
+				str[i] = str[i] - 'a' + 'A';
+		}
+		i++;
+	}
+
+	return (str);
+}
