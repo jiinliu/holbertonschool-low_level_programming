064eb3aaf34dbb00e834e0a4efb09c83e2a50094
Jin Liu <iamjincn@outlook.com>
2025-03-04 11:10:41 +0000

    task 5 fix 4

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -9,15 +9,15 @@
  */
 int _sqrt_helper(int n, int guess)
 {
-        if (n < 0)
-                return (-1);
-        if (n == 0 || n == 1)
-                return (n);
-        if (guess * guess == n)
-                return (guess);
-        if (guess * guess > n)
-                return (-1);
-        return (_sqrt_helper(n, guess + 1));
+	if (n < 0)
+		return (-1);
+	if (n == 0 || n == 1)
+		return (n);
+	if (guess * guess == n)
+		return (guess);
+	if (guess * guess > n)
+		return (-1);
+	return (_sqrt_helper(n, guess + 1));
 }
 
 /**
@@ -28,5 +28,5 @@ int _sqrt_helper(int n, int guess)
  */
 int _sqrt_recursion(int n)
 {
-        return (_sqrt_helper(n, 1));
+	return (_sqrt_helper(n, 1));
 }
