deda0316b2b8a8141bd741e621a142accc915a7a
Jin Liu <iamjincn@outlook.com>
2025-03-04 04:19:48 +0000

    task 4

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strpbrk - function
+ * @s: string
+ * @accept: character
+ * Return: pointer
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i] != '\0'; i++)
+		{
+			if (*s == accept[i])
+				return (s);
+		}
+		s++;
+	}
+	return (NULL);
+}
