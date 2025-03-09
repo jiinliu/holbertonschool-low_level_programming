c3ac5c1f9120143a67a2655f92150f51ec272368
Jin Liu <iamjincn@outlook.com>
2025-03-09 10:41:22 +0000

    task 1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ * @argc: count
+ * @argv: array
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char **argv)
+{
+	(void)argv;
+
+	printf("%d\n", argc - 1);
+	return (0);
+}
