308da0048e16fee81300584b1245ebc271e2362b
Jin Liu <iamjincn@outlook.com>
2025-03-09 11:25:31 +0000

    task 4 fix

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -20,12 +20,14 @@ int main(int argc, char **argv)
 
 	for (i = 1; i < argc; i++)
 	{
-		if (*argv[i] < '0' || *argv[i] > '9')
+		for (int j = 0; argv[i][j] != '\0'; j++)
 		{
-			printf("Error\n");
-			return (1);
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
-		sum += atoi(argv[i]);
 	}
 
 	printf("%d\n", sum);
