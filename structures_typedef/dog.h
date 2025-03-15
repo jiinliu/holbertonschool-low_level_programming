22a579d02862e39fbdb553ed1391aa1ffe135e8c
Jin Liu <iamjincn@outlook.com>
2025-03-15 09:51:56 +0000

    task 0 fix

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,11 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - structure
+ * @name: Name
+ * @age: Age
+ * @owner: Owner
+ */
 struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
 
 
