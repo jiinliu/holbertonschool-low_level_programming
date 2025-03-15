d46669b33fb7ba2a252e02d2c07722fec5077c67
Jin Liu <iamjincn@outlook.com>
2025-03-15 10:07:04 +0000

    task 1

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * init_dog - function
+ * @d: Pointer to dog
+ * @name: Name
+ * @age: Age
+ * @owner: Owner
+ *
+ * Return: Nothing
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
