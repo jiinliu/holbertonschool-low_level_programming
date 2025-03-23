891cb3a1110f79219b2a371ea88e76c494e21e05
Jin Liu <iamjincn@outlook.com>
2025-03-23 08:09:13 +0000

    task 2

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -3,19 +3,25 @@
 #include "variadic_functions.h"
 
 /**
- * print_numbers - Prints numbers, followed by a new line.
- * @separator: The string to be printed between numbers.
- * @n: The number of integers passed to the function.
+ * print_strings - Prints strings, followed by a new line.
+ * @separator: The string to be printed between strings.
+ * @n: The number of strings passed to the function.
  */
-void print_numbers(const char *separator, const unsigned int n, ...)
+void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
 	unsigned int i;
+	const char *str;
 
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", va_arg(args, int));
+		str = va_arg(args, const char*);
+		if (str == NULL)
+			printf("(nil)");
+		else
+			printf("%s", str);
+
 		if (i < n - 1 && separator != NULL)
 			printf("%s", separator);
 	}
