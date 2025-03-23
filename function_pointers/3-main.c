2f9cd66fbb15b6883f4b118db028cc288f2b5254
Jin Liu <iamjincn@outlook.com>
2025-03-23 07:31:01 +0000

    task 3 fix

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,16 +1,14 @@
 #include "3-calc.h"
-#include <string.h>
 
 /**
- * main - performs simple operations
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: Always 0
+ * main - Entry point, performs simple operations
+ * @argc: Number of arguments
+ * @argv: Array of arguments
+ * Return: 0 on success
  */
 int main(int argc, char *argv[])
 {
-	int num1, num2;
+	int num1, num2, result;
 	int (*operation)(int, int);
 
 	if (argc != 4)
@@ -21,29 +19,16 @@ int main(int argc, char *argv[])
 
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
-
-	if (strlen(argv[2]) != 1 ||
-		(*argv[2] != '+' && *argv[2] != '-' && *argv[2] != '*' &&
-		*argv[2] != '/' && *argv[2] != '%'))
-	{
-		printf("Error\n");
-		exit(99);
-	}
-
 	operation = get_op_func(argv[2]);
-	if (operation == NULL)
+
+	if (!operation)
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-
-	printf("%d\n", operation(num1, num2));
+	result = operation(num1, num2);
+	printf("%d\n", result);
 
 	return (0);
 }
