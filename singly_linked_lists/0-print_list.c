137c21a9054b365625c2a409d3fddc684153d272
Jin Liu <iamjincn@outlook.com>
2025-04-01 12:08:49 +0000

    task 0

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - function
+ * @h: Pointer to the head of the list.
+ *
+ * Return: The number of nodes in the list.
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%zu] %s\n", h->len, h->str);
+		}
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
