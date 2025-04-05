7eaea724fb694f6bf186bf8011e4c1e459e299c2
Jin Liu <iamjincn@outlook.com>
2025-04-05 13:07:12 +0000

    task 3 fix it

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -6,27 +6,6 @@
 #include <sys/types.h>
 
 /**
- * error_file - checks if files can be opened and handles errors
- * @file_from: file descriptor for source file
- * @file_to: file descriptor for destination file
- * @argv: arguments vector
- * Return: void
- */
-void error_file(int file_from, int file_to, char *argv[])
-{
-    if (file_from == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-        exit(98);
-    }
-    if (file_to == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-        exit(99);
-    }
-}
-
-/**
  * main - copies the content of a file to another file
  * @argc: number of arguments
  * @argv: arguments vector
@@ -47,26 +26,42 @@ int main(int argc, char *argv[])
 
     /* Open source file for reading */
     file_from = open(argv[1], O_RDONLY);
-    
+    if (file_from == -1)
+    {
+        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+        exit(98);
+    }
+
     /* Open destination file for writing, create if it doesn't exist, truncate if it does */
     file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-    
-    /* Check if files were opened successfully */
-    error_file(file_from, file_to, argv);
+    if (file_to == -1)
+    {
+        dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+        close(file_from);
+        exit(99);
+    }
 
     /* Read from source and write to destination */
-    nchars = 1024;
-    while (nchars == 1024)
+    while ((nchars = read(file_from, buf, 1024)) > 0)
     {
-        /* Read up to 1024 bytes from source file */
-        nchars = read(file_from, buf, 1024);
-        if (nchars == -1)
-            error_file(-1, 0, argv);
-            
         /* Write read bytes to destination file */
         nwr = write(file_to, buf, nchars);
-        if (nwr == -1)
-            error_file(0, -1, argv);
+        if (nwr == -1 || nwr != nchars)
+        {
+            dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+            close(file_from);
+            close(file_to);
+            exit(99);
+        }
+    }
+
+    /* Check if read failed */
+    if (nchars == -1)
+    {
+        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+        close(file_from);
+        close(file_to);
+        exit(98);
     }
 
     /* Close files and check for errors */
