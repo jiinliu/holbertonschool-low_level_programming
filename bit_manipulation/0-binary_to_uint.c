b43da6449a76b97b485ab80777b26c94cf2d71fa
Jin Liu <iamjincn@outlook.com>
2025-04-05 10:12:00 +0000

    task 0 fix

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * binary_to_uint - converts a binary number string to an unsigned int
+ * binary_to_uint - converts a binary number to an unsigned int
  * @b: pointer to a string of 0 and 1 characters
  *
- * Return: the converted number, or 0 if invalid input
+ * Return: the converted number, or 0 if b is NULL or has invalid chars
  */
 unsigned int binary_to_uint(const char *b)
 {
@@ -13,10 +13,11 @@ unsigned int binary_to_uint(const char *b)
 	if (b == NULL)
 		return (0);
 
-	while (*b)
+	while (*b != '\0')
 	{
 		if (*b != '0' && *b != '1')
 			return (0);
+
 		result = result * 2 + (*b - '0');
 		b++;
 	}
