e4865c49b827e74c196ba879288d81268cebf841
Jin Liu <iamjincn@outlook.com>
2025-04-05 12:57:31 +0000

    task 3 fix

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -46,8 +46,18 @@ int main(int argc, char *argv[])
 		print_error_and_exit(99, "Error: Can't write to %s\n", argv[2]);
 	}
 
-	while ((r = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	while (1)
 	{
+		r = read(fd_from, buffer, BUFFER_SIZE);
+		if (r == -1)
+		{
+			close(fd_from);
+			close(fd_to);
+			print_error_and_exit(98, "Error: Can't read from file %s\n", argv[1]);
+		}
+		if (r == 0)
+			break;
+
 		w = write(fd_to, buffer, r);
 		if (w == -1 || w != r)
 		{
@@ -55,13 +65,6 @@ int main(int argc, char *argv[])
 			close(fd_to);
 			print_error_and_exit(99, "Error: Can't write to %s\n", argv[2]);
 		}
-	}
-
-	if (r == -1)
-	{
-		close(fd_from);
-		close(fd_to);
-		print_error_and_exit(98, "Error: Can't read from file %s\n", argv[1]);
 	}
 
 	if (close(fd_from) == -1)
